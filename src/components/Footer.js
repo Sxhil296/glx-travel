9e748bff9ed621be2d3f95b7ea5d43600945a826
Sahil <leviack629@gmail.com>
2022-10-16 23:54:46 +0530

    last commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import {FaFacebook, FaLinkedin, FaMailBulk, FaPhone, FaSearchLocation, FaTwitter} from 'react-icons/fa'
 import './FooterStyles.css'
 
